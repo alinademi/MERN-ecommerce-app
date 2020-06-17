794a0f78dd241fe77cc0c91ea50fe6c693ac348c
ali <nademi@gmail.com>
2020-06-17 16:24:29 -0700

    validation for create product implemented image upload limit was set

--- controllers/product.js
+++ controllers/product.js
@@ -13,16 +13,39 @@ exports.create = (req, res) => {
         error: "Image could not be uploaded",
       });
     }
+
+    // Validate form fields before creating products
+    const { name, description, price, category, quantity, shipping } = fields;
+
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: "All fields are required",
+      });
+    }
+
     let product = new Product(fields); //handles form field data such as name, desc etc...
 
     if (files.photo) {
+      // sets a limit on the size of image to upload
+      if (files.photo.size > 512000) {
+        return res.status(400).json({
+          error: "Image size cannot not be bigger than 512 kb",
+        });
+      }
       product.photo.data = fs.readFileSync(files.photo.path); //handles image data from form
       product.photo.contentType = files.photo.type;
     }
 
     product.save((err, result) => {
       if (err) {
-        return res.staus(400).json({
+        return res.status(400).json({
           error: errorHandler(err),
         });
       }
