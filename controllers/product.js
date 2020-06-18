874ef487373ca42caf083dd8d7921e127bd1dd0d
ali <nademi@gmail.com>
2020-06-17 21:20:43 -0700

    product edit(update) controller method created,

--- controllers/product.js
+++ controllers/product.js
@@ -30,6 +30,7 @@ exports.read = (req, res) => {
 //
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
+  //parse the request
   form.keepExtensions = true;
   form.parse(req, (err, fields, files) => {
     if (err) {
@@ -39,8 +40,9 @@ exports.create = (req, res) => {
     }
 
     // Validate form fields before creating product
+    //(grab the following properties from form fields)
     const { name, description, price, category, quantity, shipping } = fields;
-
+    //check if the fields are empty
     if (
       !name ||
       !description ||
@@ -53,7 +55,7 @@ exports.create = (req, res) => {
         error: "All fields are required",
       });
     }
-
+    // if the form data passes validation then go ahead and create the product
     let product = new Product(fields); //handles form field data such as name, desc etc...
 
     if (files.photo) {
@@ -94,6 +96,62 @@ exports.remove = (req, res) => {
 
     res.json({
       message: "Product has been successfully removed",
+    });
+  });
+};
+
+// method for updating a product /////
+//
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm(); // get incoming form data
+  form.keepExtensions = true;
+  //parse the request
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Image could not be uploaded",
+      });
+    }
+
+    // Validate form fields before updating the product
+    //(grab the following properties from form fields)
+    const { name, description, price, category, quantity, shipping } = fields;
+    //check if the fields are empty
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: "All fields are required",
+      });
+    }
+    // if the form data passes validation then go ahead and edit(update) the product
+    let product = req.product; //go ahead and replace product info with the new info from form
+    //by using extend method form lodash library
+    product = _.extend(product, fields);
+
+    if (files.photo) {
+      // sets a limit on the size of image to upload
+      if (files.photo.size > 512000) {
+        return res.status(400).json({
+          error: "Image size cannot not be bigger than 512 kb",
+        });
+      }
+      product.photo.data = fs.readFileSync(files.photo.path); //handles image data from form
+      product.photo.contentType = files.photo.type;
+    }
+
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      }
+      res.json(result);
     });
   });
 };
--- routes/product.js
+++ routes/product.js
@@ -1,21 +1,42 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, productById, read, remove } = require("../controllers/product");
+const {
+  create,
+  productById,
+  read,
+  remove,
+  update,
+} = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
 //Route for getting one product by id
+//
 router.get("/product/:productId", read);
+
 //Route for creating a product
+//
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
+
 //Route for deleting a product
+//
 router.delete(
   "/product/:productId/:userId",
   requireSignin,
   isAuth,
   isAdmin,
   remove
+);
+
+//Route for deleting a product
+//
+router.put(
+  "/product/:productId/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  update
 );
 
 // if there is user is in the route as parameter,
