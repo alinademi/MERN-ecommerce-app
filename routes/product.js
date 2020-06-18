a3635d5097663f7e29655d54b3aa2da300446601
ali <nademi@gmail.com>
2020-06-17 17:14:39 -0700

    get method and route  for product by id created

--- routes/product.js
+++ routes/product.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/product");
+const { create, productById, read } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
+router.get("/product/:productId", read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
 
 // if there is user is in the route as parameter,
 // then run userById and populate the user and add product
 router.param("userId", userById);
+
+router.param("productId", productById);
 
 module.exports = router;
