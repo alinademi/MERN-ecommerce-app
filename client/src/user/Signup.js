7bd195459649d2a3bbe1c7e14dcad1b801039936
ali <nademi@gmail.com>
2020-06-20 16:47:25 -0700

    user signup with auth completed, refactored the signup page as well

--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Layout from "../core/Layout";
-import { API } from "../config";
+import { signup } from "../auth";
 
 const Signup = () => {
   const [values, setValues] = useState({
@@ -11,8 +12,29 @@ const Signup = () => {
     success: false,
   });
 
+  const { name, email, password, success, error } = values;
+
   const handleChange = (name) => (event) => {
     setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password }).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, success: false });
+      } else {
+        setValues({
+          ...values,
+          name: "",
+          email: "",
+          password: "",
+          error: "",
+          success: true,
+        });
+      }
+    });
   };
 
   const signUpForm = () => (
@@ -23,6 +45,7 @@ const Signup = () => {
           onChange={handleChange("name")}
           type="text"
           className="form-control"
+          value={name}
         />
       </div>
 
@@ -32,6 +55,7 @@ const Signup = () => {
           onChange={handleChange("email")}
           type="email"
           className="form-control"
+          value={email}
         />
       </div>
 
@@ -41,20 +65,42 @@ const Signup = () => {
           onChange={handleChange("password")}
           type="password"
           className="form-control"
+          value={password}
         />
       </div>
-      <button className="btn btn-primary">Submit</button>
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Submit
+      </button>
     </form>
+  );
+
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showSuccess = () => (
+    <div
+      className="alert alert-info"
+      style={{ display: success ? "" : "none" }}
+    >
+      New account is created. Please <Link to="/signin">Signin</Link>
+    </div>
   );
 
   return (
     <Layout
       title="Signup"
       description="Signup to Node React E-commerce App"
-      className="container col-md-4 offset-md-4"
+      className="container col-md-8 offset-md-2"
     >
+      {showSuccess()}
+      {showError()}
       {signUpForm()}
-      {JSON.stringify(values)}
     </Layout>
   );
 };
