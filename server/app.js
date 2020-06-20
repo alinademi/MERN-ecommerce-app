586391083daa5faa8d6de2cb505756f644fad946
ali <nademi@gmail.com>
2020-06-19 20:19:26 -0700

    client folder added

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
+const cors = require("cors");
 
 // environment variables /////
 require("dotenv").config();
@@ -30,6 +31,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 // routes middleware /////
 app.use("/api", authRoutes);
