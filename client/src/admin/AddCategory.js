09d3ffe116e3f45b49586ba82fa7c124c38838b4
ali <nademi@gmail.com>
2020-06-21 17:02:18 -0700

    create category done, back to dashboard done

--- client/src/admin/AddCategory.js
+++ client/src/admin/AddCategory.js
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+import { createCategory } from "./apiAdmin";
 
 const AddCategory = () => {
   const [name, setName] = useState("");
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
 
-  // destructure user and token form  localstorage
-
+  // destructure user and token from localstorage
   const { user, token } = isAuthenticated();
 
   const handleChange = (e) => {
@@ -22,9 +22,17 @@ const AddCategory = () => {
     setError("");
     setSuccess(false);
     // making post request to api to create category
+    createCategory(user._id, token, { name }).then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setError("");
+        setSuccess(true);
+      }
+    });
   };
 
-  const newCategoryForm = () => (
+  const newCategoryFom = () => (
     <form onSubmit={clickSubmit}>
       <div className="form-group">
         <label className="text-muted">Name</label>
@@ -34,19 +42,46 @@ const AddCategory = () => {
           onChange={handleChange}
           value={name}
           autoFocus
+          required
         />
       </div>
       <button className="btn btn-outline-primary">Create Category</button>
     </form>
   );
 
+  const showSuccess = () => {
+    if (success) {
+      return <h3 className="text-success">{name} is created</h3>;
+    }
+  };
+
+  const showError = () => {
+    if (error) {
+      return <h3 className="text-danger">Category name already exists</h3>;
+    }
+  };
+  // handles back button to admin dashboard
+  // we can use round brackets because this is a single statement
+  const goBack = () => (
+    <div className="mt-5">
+      <Link to="/admin/dashboard" className="text-warning">
+        Back to Dashboard
+      </Link>
+    </div>
+  );
+
   return (
     <Layout
       title="Add a new category"
-      description={`Hey ${name}, wanna add a new category?`}
+      description={`Hey ${user.name}, ypu can create your categories here`}
     >
       <div className="row">
-        <div className="col-md-8 offset-md-2">{newCategoryForm()}</div>
+        <div className="col-md-8 offset-md-2">
+          {showSuccess()}
+          {showError()}
+          {newCategoryFom()}
+          {goBack()}
+        </div>
       </div>
     </Layout>
   );
---
+++ client/src/admin/apiAdmin.js
@@ -0,0 +1,20 @@
+import { API } from "../config";
+
+// method for posting category create to backend. user id is sent ot define the role and make sure it's admin.
+export const createCategory = (userId, token, category) => {
+  return fetch(`${API}/category/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(category),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
