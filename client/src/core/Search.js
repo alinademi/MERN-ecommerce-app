0e1c2f2297f751610cb2673fd484b3ad79f254a8
ali <nademi@gmail.com>
2020-06-22 21:43:52 -0700

    so far can search products with the help of query-string package must show results in card format

--- client/src/core/Search.js
+++ client/src/core/Search.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getCategories } from "./apiCore";
+import { getCategories, list } from "./apiCore";
 import Card from "./Card";
 
 const Search = () => {
@@ -27,12 +27,28 @@ const Search = () => {
     loadCategories();
   }, []);
 
-  const searchSubmit = () => {
-    //
+  const searchData = () => {
+    // console.log(search, category);
+    if (search) {
+      list({ search: search || undefined, category: category }).then(
+        (response) => {
+          if (response.error) {
+            console.log(response.error);
+          } else {
+            setData({ ...data, results: response, searched: true });
+          }
+        }
+      );
+    }
   };
 
-  const handleChange = () => {
-    //
+  const searchSubmit = (e) => {
+    e.preventDefault();
+    searchData();
+  };
+
+  const handleChange = (name) => (event) => {
+    setData({ ...data, [name]: event.target.value, searched: false });
   };
 
   const searchForm = () => (
@@ -41,7 +57,7 @@ const Search = () => {
         <div className="input-group input-group-lg">
           <div className="input-group-prepend">
             <select className="btn mr-2" onChange={handleChange("category")}>
-              <option value="All">Pick A Category</option>
+              <option value="All">Pick Category</option>
               {categories.map((c, i) => (
                 <option key={i} value={c._id}>
                   {c.name}
@@ -66,7 +82,10 @@ const Search = () => {
 
   return (
     <div className="row">
-      <div className="container mb-3">{searchForm()}</div>
+      <div className="container mb-3">
+        {searchForm()}
+        {JSON.stringify(results)}
+      </div>
     </div>
   );
 };
