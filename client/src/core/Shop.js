25f29f5f7005a502b153500d2c94f4a6e421de59
ali <nademi@gmail.com>
2020-06-22 17:52:10 -0700

    products now are being shown in cards and can be filtered based on categories and price reange next step load more button  on shoppage

--- client/src/core/Shop.js
+++ client/src/core/Shop.js
@@ -1,45 +1,112 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import Card from "./Card";
-import { getCategories } from "../admin/apiAdmin";
+import { getCategories, getFilteredProducts } from "./apiCore";
 import Checkbox from "./Checkbox";
+import RadioBox from "./RadioBox";
+import { prices } from "./fixedPrices";
 
 const Shop = () => {
-  const [categories, setCategories] = useState([]);
-  const [error, setError] = useState(false);
-
-  const init = () => {
-    getCategories().then((data) => {
-      if (data.error) {
-        setError(data.error);
-      } else {
-        setCategories(data);
-      }
+    const [myFilters, setMyFilters] = useState({
+        filters: { category: [], price: [] }
     });
-  };
+    const [categories, setCategories] = useState([]);
+    const [error, setError] = useState(false);
+    const [limit, setLimit] = useState(6);
+    const [skip, setSkip] = useState(0);
+    const [filteredResults, setFilteredResults] = useState([]);
 
-  useEffect(() => {
-    init();
-  }, []);
+    const init = () => {
+        getCategories().then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setCategories(data);
+            }
+        });
+    };
 
-  return (
-    <Layout
-      title="Shop Page"
-      description="Search and find books of your choice"
-      className="container-fluid"
-    >
-      <div className="row">
-        <div className="col-4">
-          <h4>Filter by categories</h4>
-          <ul>
-            <Checkbox categories={categories} />
-          </ul>
-        </div>
+    const loadFilteredResults = newFilters => {
+        // console.log(newFilters);
+        getFilteredProducts(skip, limit, newFilters).then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setFilteredResults(data.data);
+            }
+        });
+    };
 
-        <div className="col-8">right</div>
-      </div>
-    </Layout>
-  );
+    useEffect(() => {
+        init();
+        loadFilteredResults(skip, limit, myFilters.filters);
+    }, []);
+
+    const handleFilters = (filters, filterBy) => {
+        // console.log("SHOP", filters, filterBy);
+        const newFilters = { ...myFilters };
+        newFilters.filters[filterBy] = filters;
+
+        if (filterBy === "price") {
+            let priceValues = handlePrice(filters);
+            newFilters.filters[filterBy] = priceValues;
+        }
+        loadFilteredResults(myFilters.filters);
+        setMyFilters(newFilters);
+    };
+
+    const handlePrice = value => {
+        const data = prices;
+        let array = [];
+
+        for (let key in data) {
+            if (data[key]._id === parseInt(value)) {
+                array = data[key].array;
+            }
+        }
+        return array;
+    };
+
+    return (
+        <Layout
+            title="Shop Page"
+            description="Search and find books of your choice"
+            className="container-fluid"
+        >
+            <div className="row">
+                <div className="col-4">
+                    <h4>Filter by categories</h4>
+                    <ul>
+                        <Checkbox
+                            categories={categories}
+                            handleFilters={filters =>
+                                handleFilters(filters, "category")
+                            }
+                        />
+                    </ul>
+
+                    <h4>Filter by price range</h4>
+                    <div>
+                        <RadioBox
+                            prices={prices}
+                            handleFilters={filters =>
+                                handleFilters(filters, "price")
+                            }
+                        />
+                    </div>
+                </div>
+
+                <div className="col-8">
+                    <h2 className="mb-4">Products</h2>
+                    <div className="row">
+                        {filteredResults.map((product, i) => (
+                            <Card key={i} product={product} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
 };
 
 export default Shop;
